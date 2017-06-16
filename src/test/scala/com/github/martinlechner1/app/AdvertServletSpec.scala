e6f6172bbe73dbf07f4c114b02e2b45097f57498
martinlechner1 <martin.lechner.ml@gmail.com>
2017-06-16 11:57:25 +0200

    Added api tests.

--- src/test/scala/com/github/martinlechner1/app/AdvertServletSpec.scala
+++ src/test/scala/com/github/martinlechner1/app/AdvertServletSpec.scala
@@ -1,6 +1,8 @@
 package com.github.martinlechner1.app
 
-import com.github.martinlechner1.database.{AdvertDAO, Id}
+import java.time.Instant
+
+import com.github.martinlechner1.database.{AdvertDAO, Id, Mileage}
 import com.github.martinlechner1.model.{Advert, Diesel}
 import org.mockito.Mockito.reset
 import org.scalatra.test.specs2._
@@ -27,13 +29,34 @@ class AdvertServletSpec extends MutableScalatraSpec with JsonMatchers with Mocki
 
 
   private val NEW_AUDI = Advert(1, "Audi A4", Diesel, 10, true, Option.empty, Option.empty)
+  private val USED_AUDI = Advert(1, "Audi A4", Diesel, 10, false, Option(1), Option(Instant.parse("2005-12-24T18:21:30.00Z")))
+
 
   "GET / on AdvertServlet" should {
     "return a list of CarAdverts" in {
       advertDAO.getAll(Id).returns(List(NEW_AUDI))
       get(servletPath) {
+        status must_== 200
         response.body must /#(0) / ("id" -> 1)
         response.body must /#(0) / ("title" -> "Audi A4")
+      }
+    }
+    "allows sorting" in {
+      advertDAO.getAll(Mileage).returns(List(NEW_AUDI))
+      get(servletPath + "?sort=mileage") {
+        status must_== 200
+        response.body must /#(0) / ("id" -> 1)
+        response.body must /#(0) / ("title" -> "Audi A4")
+        there was one(advertDAO).getAll(Mileage)
+      }
+    }
+    "allows sorting fallback to Id" in {
+      advertDAO.getAll(Id).returns(List(NEW_AUDI))
+      get(servletPath + "?sort=abc") {
+        status must_== 200
+        response.body must /#(0) / ("id" -> 1)
+        response.body must /#(0) / ("title" -> "Audi A4")
+        there was one(advertDAO).getAll(Id)
       }
     }
   }
@@ -80,7 +103,72 @@ class AdvertServletSpec extends MutableScalatraSpec with JsonMatchers with Mocki
         there was no(advertDAO).create(any[Advert])
       }
     }
+    "fail with 400 on negative price" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": -10, "new": true}"""
+      post(servletPath, headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).create(any[Advert])
+      }
+    }
+    "fail with 400 if firstRegistration is set on a new car" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": -10, "new": true, "firstRegistration": "2005-12-24T18:21:30.00Z"}}"""
+      post(servletPath, headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).create(any[Advert])
+      }
+    }
+    "fail with 400 if mileage is set on a new car" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": -10, "new": true, "mileage": 1}}"""
+      post(servletPath, headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).create(any[Advert])
+      }
+    }
+    "accept correct used car" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2005-12-24T18:21:30.00Z", "mileage": 1}"""
+      advertDAO.create(any[Advert]).returns(1)
+      post(servletPath, headers = jsonHeaders, body = requestBody) {
+        status must_== 200
+        there was one(advertDAO).create(USED_AUDI)
+      }
+    }
+    "fail with 400 on missing mileage if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2005-12-24T18:21:30.00Z"}"""
+      post(servletPath, headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).create(any[Advert])
+      }
+    }
+    "fail with negative mileage if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2005-12-24T18:21:30.00Z", "mileage": -10}"""
+      post(servletPath, headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).create(any[Advert])
+      }
+    }
+    "fail with 0 mileage if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2005-12-24T18:21:30.00Z", "mileage": 0}"""
+      post(servletPath, headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).create(any[Advert])
+      }
+    }
+    "fail with 400 on missing firstRegistration if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "mileage": 1}"""
+      post(servletPath, headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).create(any[Advert])
+      }
+    }
+    "fail with 400 on future firstRegistration if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2100-12-24T18:21:30.00Z", "mileage": 1}"""
+      post(servletPath, headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).create(any[Advert])
+      }
+    }
   }
+
 
   "PUT /:id on AdvertServlet" should {
     val jsonHeaders = Map("Accept" -> "application/json", "Content-Type" -> "application/json")
@@ -110,6 +198,70 @@ class AdvertServletSpec extends MutableScalatraSpec with JsonMatchers with Mocki
     }
     "fail with 400 on empty title" in {
       val requestBody = """{"id": 1, "title": ""}"""
+      put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).update(any[Advert])
+      }
+    }
+    "fail with 400 on negative price" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": -10, "new": true}"""
+      put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).update(any[Advert])
+      }
+    }
+    "fail with 400 if firstRegistration is set on a new car" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": -10, "new": true, "firstRegistration": "2005-12-24T18:21:30.00Z"}}"""
+      put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).update(any[Advert])
+      }
+    }
+    "fail with 400 if mileage is set on a new car" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": -10, "new": true, "mileage": 1}}"""
+      put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).update(any[Advert])
+      }
+    }
+    "accept correct used car" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2005-12-24T18:21:30.00Z", "mileage": 1}"""
+      advertDAO.update(any[Advert]).returns(1)
+      put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
+        status must_== 200
+        there was one(advertDAO).update(USED_AUDI)
+      }
+    }
+    "fail with 400 on missing mileage if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2005-12-24T18:21:30.00Z"}"""
+      put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).update(any[Advert])
+      }
+    }
+    "fail with negative mileage if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2005-12-24T18:21:30.00Z", "mileage": -10}"""
+      put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).update(any[Advert])
+      }
+    }
+    "fail with 0 mileage if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2005-12-24T18:21:30.00Z", "mileage": 0}"""
+      put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).update(any[Advert])
+      }
+    }
+    "fail with 400 on missing firstRegistration if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "mileage": 1}"""
+      put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
+        status must_== 400
+        there was no(advertDAO).update(any[Advert])
+      }
+    }
+    "fail with 400 on future firstRegistration if not new" in {
+      val requestBody = """{"id": 1, "title": "Audi A4", "fuel": "diesel", "price": 10, "new": false, "firstRegistration": "2100-12-24T18:21:30.00Z", "mileage": 1}"""
       put(servletPathId(1), headers = jsonHeaders, body = requestBody) {
         status must_== 400
         there was no(advertDAO).update(any[Advert])
